e042895562e56f076aab80a02566574de02c42d3
plewekat <59675811+plewekat@users.noreply.github.com>
2020-02-25 00:53:05 -0800

    Update 4js.js

--- 4js.js
+++ 4js.js
@@ -2,5 +2,5 @@ const hambutton = document.querySelector(".ham");
 hambutton.addEventListener("click", toggleMenu, false);
 
 function toggleMenu() {
-  document.querySelector(".navigation")[0].classList.toggle("responsive");
+  document.querySelector(".navigation").classList.toggle("responsive");
 }
