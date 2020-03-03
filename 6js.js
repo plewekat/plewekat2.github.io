36ef79ef0d94062ae9860d3477ed91a79d302bb1
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-02 23:23:57 -0800

    Update 6js.js

--- 6js.js
+++ 6js.js
@@ -1,3 +1,4 @@
+
 const t = parseFloat(document.getElementById('t').textContent);
 const s = parseFloat(document.getElementById('s').textContent);
 document.getElementById('f').textContent = (35.74+0.6215*t-35.75*Math.pow(s, 0.16)+0.4275*t*Math.pow(s, 0.16)).toFixed(1);
