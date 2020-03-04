9bfdcb46a6fc8a907d8372f257792c6837c3ef29
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-03 23:04:45 -0800

    Update lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -1,17 +1,17 @@
-const imagesToLoad = document.querySelectorAll("[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => { image.removeAttribute("data-src");
+  image.onload = () => {image.removeAttribute("data-src");
   };
 };
 
 const imgOptions = {
-threshold: 0,
+threshold: 1,
 rootMargin: "0px 0px 50px 0px"
 };
 
 if ("IntersectionObserver" in window) {
-const observer = new IntersectionObserver((items, observer)) => {
+const observer = new IntersectionObserver((items, observer) => {
   items.forEach((item) => {
   if(item.isIntersecting) {
     loadImages(item.target);
@@ -25,7 +25,7 @@ observer.observe(img)
 });                     
 }
 else {
-  imagesToLoad.forEach((img => {
+  imagesToLoad.forEach((img) => {
   loadImages(img);
 });
 }
