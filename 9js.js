11eb0b72daa2689df6e0d2700ec49024e915ad9f
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-14 00:54:23 -0700

    Create 9js.js

---
+++ 9js.js
@@ -0,0 +1,49 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {image.removeAttribute("data-src");
+  };
+};
+
+const imgOptions = {
+threshold: 1,
+rootMargin: "0px 0px 50px 0px"
+};
+
+if ("IntersectionObserver" in window) {
+const observer = new IntersectionObserver((items, observer) => {
+  items.forEach((item) => {
+  if(item.isIntersecting) {
+    loadImages(item.target);
+    observer.unobserve(item.target);
+  }
+  });
+});
+
+imagesToLoad.forEach((img) => {
+observer.observe(img)
+});                     
+} else {
+  imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
+}
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+function myFunction(elmnt,clr) {
+  elmnt.style.color = clr;
+}
+
+WebFont.load({
+  google: {
+    families: [
+       'Montserrat'
+    ]
+  }
+});
