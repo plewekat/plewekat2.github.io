8a69b7ab765c95c09cc3683ce39941f0006b10b0
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-18 13:02:05 -0700

    Update prophets.js

--- prophets.js
+++ prophets.js
@@ -4,15 +4,16 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject)for (let i = 0; i < prophets.length; i++ ) {;  // temporary checking for valid response and data parsing
-  });
-  
   const prophets = jsonObject['prophets'];
-  
-  let card = document.createElement('section');
-let h2 = document.createElement('h2');
+   propehts.forEach(prophet => {
+   let card = document.createElement("section");
+   let h2 = document.createElement("h2");
+   }
+                    
+h2.textContent = `${prophets.name} {prophets.lastname}`;
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+)}
+)}
 
 card.appendChild(h2);
 
