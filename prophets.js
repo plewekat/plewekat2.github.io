6fe74af5f1c5de504be0753ba05e1bad02d4fe23
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-18 13:57:19 -0700

    Update prophets.js

--- prophets.js
+++ prophets.js
@@ -1,18 +1,17 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-  const prophets = jsonObject['prophets'];
-   propehts.forEach(prophet => {
-   let card = document.createElement('section');
-   let h2 = document.createElement('h2');
-                    
-h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    console.table(jsonObject);
+  for (let i = 0; i < prophets.length; i++ ) {
+    let card = document.createElement('section');
+let h2 = document.createElement('h2');
+
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
 card.appendChild(h2);
-
-document.querySelector('.cards').appendChild(card);
-   });
-});
+  }
+}
