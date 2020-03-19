ea46cca3b277aa049dd5afe9cf8957ce2cdda167
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-18 19:50:51 -0700

    Update 9js.js

--- 9js.js
+++ 9js.js
@@ -1,3 +1,36 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+  const members = jsonObject['prophets'];
+   members.forEach(prophet => {
+ 
+   let card = document.createElement('section');
+   let h2 = document.createElement('h2');
+   let image = document.createElement('img');
+   let fullname = `${prophet.name} "${prophet.lastname}`;
+   let birthday = document.createElement('birthdate');
+   let birthplace = document.createElement('birthplace');
+     if (prophet.name == "Joseph" || prophet.name == "John") {
+     
+     h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+     birthday.innerHTML = `Date of birth: ${prophet.birthdate} <br>`;
+     birthplace.innerHTML = `Place of birth: ${prophet.birthplace}`;
+     image.setAttribute('src', prophet.imageurl);
+     image.setAttribute('alt', fullname);
+     
+     card.appendChild(h2);
+     card.appendChild(birthday);
+     card.appendChild(birthplace);
+     card.appendChild(image);
+
+   document.querySelector('.cards').appendChild(card);
+     }
+     });
+}); 
+
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 const loadImages = (image) => {
   image.setAttribute("src", image.getAttribute("data-src"));
@@ -47,3 +80,4 @@ WebFont.load({
     ]
   }
 });
+
