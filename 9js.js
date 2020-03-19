02ac9d55ae5733fec4d43098762fc7e03a5ffe40
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-18 23:20:10 -0700

    Update 9js.js

--- 9js.js
+++ 9js.js
@@ -1,83 +1,37 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-  const members = jsonObject['prophets'];
-   members.forEach(prophet => {
- 
+  console.table(jsonObject);
+  
+  const towns = jsonObject['towns'];
+  
+   for (let i = 0; i < towns.length; i++) {
+     if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" || towns[i].name == "Soda Springs" ){ 
    let card = document.createElement('section');
    let h2 = document.createElement('h2');
-   let image = document.createElement('img');
-   let fullname = `${prophet.name} "${prophet.lastname}`;
-   let birthday = document.createElement('birthdate');
-   let birthplace = document.createElement('birthplace');
-     if (prophet.name == "Joseph" || prophet.name == "John") {
+   let h3 = document.createElement('h3');
+   let yearFounded = document.createElement('p');
+   let population = document.createElement('p');   
+   let rainfall = document.createElement('p');
      
-     h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-     birthday.innerHTML = `Date of birth: ${prophet.birthdate} <br>`;
-     birthplace.innerHTML = `Place of birth: ${prophet.birthplace}`;
-     image.setAttribute('src', prophet.imageurl);
-     image.setAttribute('alt', fullname);
+     h2.textContent = towns[i].name;
+     h3.textContent = towns[i].motto;
+     yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+     population.textContent = "Population: " + towns[i].currentPopulation;
+     rainfall.textContent = "Annual Rainfall: " + towns[i].averageRainfall;
      
      card.appendChild(h2);
-     card.appendChild(birthday);
-     card.appendChild(birthplace);
-     card.appendChild(image);
-
-   document.querySelector('.cards').appendChild(card);
-     }
-     });
+     card.appendChild(h3);
+     card.appendChild(yearFounded);
+     card.appendChild(population);
+     card.appendChild(rainfall);
+   
+   document.querySelector('div.cards').appendChild(card);
+}
+}
 }); 
-
-let imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {image.removeAttribute("data-src");
-  };
-};
-
-const imgOptions = {
-threshold: 1,
-rootMargin: "0px 0px 50px 0px"
-};
-
-if ("IntersectionObserver" in window) {
-const observer = new IntersectionObserver((items, observer) => {
-  items.forEach((item) => {
-  if(item.isIntersecting) {
-    loadImages(item.target);
-    observer.unobserve(item.target);
-  }
-  });
-});
-
-imagesToLoad.forEach((img) => {
-observer.observe(img)
-});                     
-} else {
-  imagesToLoad.forEach((img) => {
-  loadImages(img);
-});
-}
-
-const hambutton = document.querySelector(".ham");
-hambutton.addEventListener("click", toggleMenu, false);
-
-function toggleMenu() {
-  document.querySelector(".navigation").classList.toggle("responsive");
-}
-
-function myFunction(elmnt,clr) {
-  elmnt.style.color = clr;
-}
-
-WebFont.load({
-  google: {
-    families: [
-       'Montserrat'
-    ]
-  }
-});
 
