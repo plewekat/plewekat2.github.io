c02acabdaeb678abbb38467788998314b9297f11
plewekat <59675811+plewekat@users.noreply.github.com>
2020-03-21 07:33:08 -0700

    Create preston-10.js

---
+++ preston-10.js
@@ -0,0 +1,22 @@
+const t = parseFloat(document.getElementById('t').textContent);
+const s = parseFloat(document.getElementById('s').textContent);
+document.getElementById('f').textContent = (35.74+0.6215*t-35.75*Math.pow(s, 0.16)+0.4275*t*Math.pow(s, 0.16)).toFixed(1);
+
+const hambutton = document.querySelector(".ham");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+  document.querySelector(".navigation").classList.toggle("responsive");
+}
+
+function myFunction(elmnt,clr) {
+  elmnt.style.color = clr;
+}
+
+WebFont.load({
+  google: {
+    families: [
+       'Montserrat'
+    ]
+  }
+});
